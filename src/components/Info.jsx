d1791b68ef740a897e0431e4899b840c8cedc918
Ebanusssssi <kolisnichenkov@icloud.com>
2024-12-26 06:14:32 +0000

    instructions update

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -34,7 +34,7 @@ const InfoIcon = () => {
         >
           <ul className="text-start">
             <li className="text-sm text-white/80">Просто текст для заголовкiв <span className="text-white font-semibold">&quot;Заголовок&quot;</span></li>
-            <li className="text-sm text-white/80"><span className="text-white font-semibold">&quot;- (мiнус)&quot;</span> для пунктiв списку <span className="text-white font-semibold">&quot;-Пункт&quot;</span></li>
+            <li className="text-sm text-white/80"><span className="text-white font-semibold">Мiнус &quot;-&quot;</span> для пунктiв списку <span className="text-white font-semibold">&quot;-Пункт&quot;</span></li>
             <li className="text-sm text-white/80">Кожен <span className="text-white font-semibold">&quot;тег&quot;</span> з <span className="text-white font-semibold">нового рядка</span></li>
           </ul>
         </div>
