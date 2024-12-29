4e439bf33747498563e7aef978355c3d6dd84fd1
Ebanusssssi <kolisnichenkov@icloud.com>
2024-12-29 02:29:57 +0000

    logo now redirects you to owners site

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import Logo from "../assets/logo.webp"
 const Header = () => {
   return (
     <div className="flex flex-col items-center justify-center bg-[#191919]">
-      <img src={Logo} alt="" className="w-32"/>
+      <a href="https://heavyduty.com.ua" className=""><img src={Logo} alt="" className="w-32"/></a>
     </div>
   )
 }
